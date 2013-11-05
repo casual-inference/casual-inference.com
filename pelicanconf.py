9a4df5feb8e4b0bf3e1312ef2adf0185a41443a7
Michael Lerch <mdlerch@gmail.com>
2013-11-05 10:55:32 -0700

    initial import

---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Michael Lerch'
+SITENAME = 'Casual Inference'
+SITEURL = ''
+
+TIMEZONE = 'US/Mountain'
+
+DEFAULT_LANG = 'en'
+
+# RELATIVE_URLS = False
+RELATIVE_URLS = True
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+THEME = '/home/mike/work/web/pelican-themes/gum/'
+
+# Blogroll
+# LINKS =  (('Pelican', 'http://getpelican.com/'),
+          # ('Python.org', 'http://python.org/'),
+          # ('Jinja2', 'http://jinja.pocoo.org/'),
+          # ('You can modify those links in your config file', '#'),)
+
+# Social widget
+# SOCIAL = (('You can add links in your config file', '#'),
+          # ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
